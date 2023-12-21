ffaa303c98b27c3bf59058ab4ae925485d3aa6f8
Debasish Nandi <debasish9565@gmail.com>
2023-12-21 12:17:23 +0530

    updates

---
+++ cart/cart.js
@@ -0,0 +1,22 @@
+const product = [
+  {
+    id: 0,
+    Image: "./img/products/f1.jpg",
+    price: 378,
+  },
+  {
+    id: 1,
+    Image: "./img/products/f2.jpg",
+    price: 400,
+  },
+  {
+    id: 2,
+    Image: "./img/products/f3.jpg",
+    price: 420,
+  },
+  {
+    id: 3,
+    Image: "./img/products/f4.jpg",
+    price: 400,
+  },
+];
